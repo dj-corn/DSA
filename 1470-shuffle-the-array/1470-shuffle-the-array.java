f4ab9db749fece1cbff95291d908f32ba2e26442
Dhananjay Sharma <sha373845@gmail.com>
2026-08-19 12:58:33 +0530

    Time: 5 ms (0.65%), Space: 46.4 MB (63.14%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+        // Method ---> 1
+        // Method ---> 1
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            arr.add(nums[i]);
+            arr.add(nums[i+n]);
+        }
+        int[] ans = arr.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
+        
+
+        // Method ---> 2
+        // Method ---> 2
+        // int [] ans = new int[2*n];
+        // int j =0;
+        // for(int i=0; i<2*n; i++){
+        //     ans[i] = nums[j];
+        //     ans[i+1] = nums[j+n];
+        //     j++;
+        //     i++;
+        // }
+        // return ans;
+    }
+}
